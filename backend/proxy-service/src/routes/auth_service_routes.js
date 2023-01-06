7ffaebe12d6311b07462f97105ce6d8e83c5c5c3
iRobot98 <matendejudah@gmail.com>
2023-01-06 06:25:23 +0300

    updating remote

--- backend/proxy-service/src/routes/auth_service_routes.js
+++ backend/proxy-service/src/routes/auth_service_routes.js
@@ -1,13 +1,29 @@
 module.exports = (fastify = require("fastify")({ logger: true })) => {
+  const app = "proxy-service";
+  const service = "accounts";
+
   fastify.get("/accounts/", async (request, reply) => {
     reply
       .code(200)
       .header("Content-Type", "application/json; charset=utf-8")
       .send({
-        app: "proxy-service",
-        service: "accounts",
+        app,
+        service,
         data: {
           data: "hello world",
+        },
+      });
+  });
+  fastify.post("/accounts/", async (request, reply) => {
+    console.log(request.body);
+    reply
+      .code(200)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({
+        app,
+        service,
+        data: {
+          data: "hello world from post api",
         },
       });
   });
